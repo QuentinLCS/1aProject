ba31c7c63f8adfc86a710021ef5006dc6702d7e5
Quentin LCS <quentin.lechasles@gmail.com>
2018-11-14 13:31:00 +0100

    Continuité du plateau

--- Sources/plateau.c
+++ Sources/plateau.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
+#include "../Header/jeu.h"
 #define TAILLE_PLATEAU 15
 #define TAILLE_ECURIE 6
 
 void main()
 {
     int i, j;
-    //plateau cases[];
+    plateau matrice[][4] ={ {0,1,2,3} , {4,5,6,7} };
 
     for (i = 0; i < TAILLE_PLATEAU; i++) {
-        for(j = 0; j < TAILLE_PLATEAU; j++)
-        {
+        for(j = 0; j < TAILLE_PLATEAU; j++) {
             if (j < TAILLE_ECURIE && i < TAILLE_ECURIE) {
                 printf("1 ");
             } else if (j >= TAILLE_PLATEAU - TAILLE_ECURIE && i < TAILLE_ECURIE) {
@@ -18,14 +18,17 @@ void main()
                 printf("3 ");
             } else if (j >= TAILLE_PLATEAU - TAILLE_ECURIE && i >= TAILLE_PLATEAU - TAILLE_ECURIE) {
                 printf("4 ");
-            } else 
-            if (j == TAILLE_PLATEAU / 2 && TAILLE_PLATEAU % 2 != 0 || TAILLE_PLATEAU % 2 == 0 && (j == TAILLE_PLATEAU / 2 || j == TAILLE_PLATEAU / 2 + 1) ) {
+            } else if (j == TAILLE_PLATEAU / 2 && TAILLE_PLATEAU % 2 != 0 || TAILLE_PLATEAU % 2 == 0 && (j == TAILLE_PLATEAU / 2 || j == TAILLE_PLATEAU / 2 + 1) ) {
                 printf("9 ");
-            }
-            
-            else {
+            } else {
                 printf("0 ");
             } 
         } printf("\n");
+    }
+
+    for (i = 0; i < TAILLE_PLATEAU; i++) {
+        for(j = 0; j < TAILLE_PLATEAU; j++) {
+            printf("%d \n", matrice[i][j]);
+        } 
     }
 }
