b7dc5043e42b71327da2b66bc3fdd80dc0f7ff8f
Quentin LCS <quentin.lechasles@gmail.com>
2018-11-14 21:04:29 +0100

    Mise à jour du plateau. Ca avance !

--- Sources/plateau.c
+++ Sources/plateau.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "../Header/jeu.h"
-#define TAILLE_PLATEAU 15
-#define TAILLE_ECURIE 6
+#define TAILLE_PLATEAU 15 // IMPAIR 
+#define TAILLE_ECURIE 6 // Calculable ?
 
 // void creation_plateau()
 void main()
@@ -19,7 +19,9 @@ void main()
                 matrice[i][j] = 3;
             } else if (j >= TAILLE_PLATEAU - TAILLE_ECURIE && i >= TAILLE_PLATEAU - TAILLE_ECURIE) {
                 matrice[i][j] = 4;
-            } else if (j == TAILLE_PLATEAU / 2 && TAILLE_PLATEAU % 2 != 0 || TAILLE_PLATEAU % 2 == 0 && (j == TAILLE_PLATEAU / 2 || j == TAILLE_PLATEAU / 2 + 1) ) {
+            } else if (j == TAILLE_PLATEAU / 2 && i == TAILLE_PLATEAU / 2 && TAILLE_PLATEAU % 2 != 0 || j == TAILLE_PLATEAU / 2 && i == TAILLE_PLATEAU / 2 && TAILLE_PLATEAU % 2 == 0) {
+                matrice[i][j] = 7;
+            } else if (((j == TAILLE_PLATEAU / 2 && TAILLE_PLATEAU % 2 != 0) || (i == TAILLE_PLATEAU / 2 && TAILLE_PLATEAU % 2 != 0)) && (i != 0 && i != TAILLE_PLATEAU - 1 && j != 0 && j != TAILLE_PLATEAU - 1)) {
                 matrice[i][j] = 9;
             } else {
                 matrice[i][j] = 0;
