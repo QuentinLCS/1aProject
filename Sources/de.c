356e948fa6b3667f6565b232ae6d484bbfadf89e
Quentin LCS <quentin.lechasles@gmail.com>
2018-11-15 13:01:26 +0100

    Modification du dé. Possible d'utiliser plusieurs dés.

--- Sources/de.c
+++ Sources/de.c
@@ -2,9 +2,14 @@
 #include <stdlib.h>
 #include <time.h>
 
-//int de()
-int main()
+//int de(int nbDe)
+int main(int nbDe)
 {
+    int max, min;
     srand(time(NULL));
+
+    min = nbDe;
+    max = nbDe * 6;
+    
     return rand() % (max + 1 - min) + min;
 }
