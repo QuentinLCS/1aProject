c4c554e94edbd09438959a5c0ee52c1fb06c3b8f
Nathan <nathan.chavas@gmail.com>
2018-11-15 15:19:16 +0100

    .

--- Sources/menu.c
+++ Sources/menu.c
@@ -6,24 +6,42 @@ void menu()
 {
     int gameMode, nbJoueurs;
 
-    printf("\n[Projet 1A] PETITS CHEVAUX \nPar : CHAVAS Nathan & LECHASLES Quentin\n\nChoisissez votre mode de jeu :\n  [1] Mode SEUL\n  [2] Mode MULTIJOUEUR\n  [3] Mode IA \n\nMode [entrez une valeur]: ");
+    printf("\n[Projet 1A] PETITS CHEVAUX \nPar : CHAVAS Nathan & LECHASLES Quentin\n\nChoisissez votre mode de jeu :\n  [1] Nouvelle partie\n  [2] Continuer\n  [3] Options\n  [4] Quitter \n\nMode [entrez une valeur]: ");
     scanf("%d", &gameMode);
 
     
     switch (gameMode) {
         case 1 :
-            printf("\n\n[v] Mode SEUL : SELECTIONNE !\n\n");
-            solo();
+            printf("\n\n[v] NOUVELLE PARTIE : SELECTIONNE !\n\n");
+            NvPartie();
             break;
         case 2 :
-            printf("\n\n[v] Mode MULTIJOUEUR : SELECTIONNE !\n\n");
-            multi();
+            printf("\n\n[v] CONTINUER : SELECTIONNE !\n\n");
+            Continue();
             break;
         case 3 :
-            printf("\n\n[v] Mode IA : SELECTIONNE !\n\n");
-            IA();
+            printf("\n\n[v] OPTION : SELECTIONNE !\n\n");
+            Option();
+            break;
+        case 4 :
+            printf("\n\n[v] QUITTER : SELECTIONNE !\n\n");
             break;
     }
+}
+
+int NvPartie (){
+
+    return 0;
+}
+
+int Continue (){
+
+    return 0;
+}
+
+int Option (){
+
+    return 0;
 }
 
 int solo() {
