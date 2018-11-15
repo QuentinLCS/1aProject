7359ab60515557facddd3505f9b94206f422cb38
Quentin LCS <quentin.lechasles@gmail.com>
2018-11-15 15:24:30 +0100

    :)

--- Sources/menu.c
+++ Sources/menu.c
@@ -2,41 +2,23 @@
 #include "../Header/jeu.h"
 
 // void menu()
-void menu()
+void main()
 {
     int gameMode, nbJoueurs;
 
-    printf("\n[Projet 1A] PETITS CHEVAUX \nPar : CHAVAS Nathan & LECHASLES Quentin\n\nChoisissez votre mode de jeu :\n  [1] Mode SEUL\n  [2] Mode MULTIJOUEUR\n  [3] Mode IA \n\nMode [entrez une valeur]: ");
+    printf("\n[Projet 1A] PETITS CHEVAUX \nPar : CHAVAS Nathan & LECHASLES Quentin\n\nQue souhaitez-vous faire ?\n  [1] JOUER\n  [2] SCENARIOS\n  [3] OPTIONS\n  [4] QUITTER\n\nMode [entrez une valeur]: ");
     scanf("%d", &gameMode);
 
     
     switch (gameMode) {
         case 1 :
             printf("\n\n[v] Mode SEUL : SELECTIONNE !\n\n");
-            solo();
             break;
         case 2 :
             printf("\n\n[v] Mode MULTIJOUEUR : SELECTIONNE !\n\n");
-            multi();
             break;
         case 3 :
             printf("\n\n[v] Mode IA : SELECTIONNE !\n\n");
-            IA();
             break;
     }
-}
-
-int solo() {
-
-    return 0;
-}
-
-int multi() {
-
-    return 0;
-}
-
-int IA () {
-
-    return 0;
 }
