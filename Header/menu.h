7608ec611cd2f19ab22cd58f1caba9031ed08873
Quentin LCS <quentin.lechasles@gmail.com>
2018-11-16 23:12:14 +0100

    Modifications minimes du menu.

---
+++ Header/menu.h
@@ -0,0 +1,8 @@
+#if !defined(MENU_H)
+#define MENU_H
+
+void menu();
+int nouvellePartie();
+void option();
+
+#endif // MENU_H
--- Sources/menu.c
+++ Sources/menu.c
@@ -1,72 +1,70 @@
 #include <stdio.h>
 #include "../Header/jeu.h"
+#include "../Header/menu.h"
 
 // void menu()
 void main()
 {
-    int gameMode, valide = 0;
+    int gameMode = 0, valide = 0;
 
     do {
         printf("\n[Projet 1A] PETITS CHEVAUX \nPar : CHAVAS Nathan & LECHASLES Quentin\n\nChoisissez votre mode de jeu :\n  [1] Nouvelle partie\n  [2] Continuer\n  [3] Options\n  [4] Quitter \n\nMode [entrez une valeur]: ");
         valide = scanf("%d", &gameMode);
-    } while ( (gameMode < 1 || gameMode > 4) && valide == 0 );
+    } while ( gameMode < 1 || gameMode > 4 || valide == 0 );
     
     switch (gameMode) {
         case 1 :
-            printf("\n\n[v] NOUVELLE PARTIE : SELECTIONNE !\n\n");
+            printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n[v] NOUVELLE PARTIE : SELECTIONNE !\n\n");
             //init();
             break;
         case 2 :
-            printf("\n\n[v] CONTINUER : SELECTIONNE !\n\n");
-            //Continue();
+            printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n[v] CONTINUER : SELECTIONNE !\n\n");
+            //continuer();
             break;
         case 3 :
-            printf("\n\n[v] OPTION : SELECTIONNE !\n\n");
-            Option();
+            printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n[v] OPTIONS : SELECTIONNE !\n\n");
+            option();
             break;
         case 4 :
-            printf("\n\n[v] QUITTER : SELECTIONNE !\n\n");
+            printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n[v] QUITTER : SELECTIONNE !\n\n");
             break;
     }
 }
 
-int NvPartie (){
+int nouvellePartie (){
 
-    int nbJoueur, valide = 0;
+    int nbJoueur = 0, valide = 0;
 
     do {
         printf ("À combien de joueur voulez-vous jouer ? \n --> 1 JOUEUR\n\n --> 2 JOUEURS\n\n --> 3 JOUEURS\n\n --> 4 JOUEURS\n\n");
         valide = scanf ("%d",&nbJoueur);
-    } while ( (nbJoueur < 1 || nbJoueur > 4) && valide == 0 );
+    } while ( nbJoueur < 1 || nbJoueur > 4 || valide == 0 );
     printf ("\n\n[Nombre %d Joueur(s) séléctionné]\n\n", nbJoueur);
     //Jeu();
     return nbJoueur;
 }
 
-int Continue (){
+void continuer (){
     //Jeu();
-    return 0;
 }
 
-int Option (){
+void option (){
 
-    int optionNb, valide = 0;
-
-    printf("\n[OPTIONS] A quelle option souhaitez-vous accéder ?\n  [1] Scénarios\n  [2] xxx\n  [3] xxx\n\nOption [entrez une valeur]: ");
-    scanf("%d", &optionNb);
-
+    int optionNb = 0, valide = 0;
+    
+    do {
+        printf("\n[OPTIONS] A quelle option souhaitez-vous accéder ?\n  [1] Scénarios\n  [2] xxx\n  [3] xxx\n\nOption [entrez une valeur]: ");
+        valide == scanf("%d", &optionNb);
+    } while ( optionNb < 1 || optionNb > 3 || valide == 0 );
     switch (optionNb) {
         case 1 :
-            printf("\n\n[v] NOUVELLE PARTIE : SELECTIONNE !\n\n");
+            printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n[v] SCENARIOS : SELECTIONNE !\n\n");
             break;
         case 2 :
-            printf("\n\n[v] CONTINUER : SELECTIONNE !\n\n");
+            printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n[v] CONTINUER : SELECTIONNE !\n\n");
             break;
         case 3 :
-            printf("\n\n[v] OPTION : SELECTIONNE !\n\n");
-            break;
-        case 4 :
-            printf("\n\n[v] QUITTER : SELECTIONNE !\n\n");
+            printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n[v] OPTIONS : SELECTIONNE !\n\n");
             break;
     }
 }
