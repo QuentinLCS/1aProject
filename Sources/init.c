284eb1fdeb6f7c93ec697b803e6b29d4909ebade
Nathan <nathan.chavas@gmail.com>
2018-11-16 22:36:44 +0100

    .

--- Sources/init.c
+++ Sources/init.c
@@ -4,10 +4,17 @@
 void init (){
     Joueur Players[4];
     Players[0].num_joueur=1;
-    Players[0].couleur="Bleu"
+    Players[0].couleur="bleu";
+    Players[0].num_joueur=2;
+    Players[1].couleur="rouge";
+    Players[0].num_joueur=3;
+    Players[2].couleur="jaune";
+    Players[0].num_joueur=4;
+    Players[3].couleur="vert";
+
 
     int nbJoueur=nbJoueur();
     for (int i=0; i<nbJoueur; i++){
-        printf ("\nEntrez le nom du joueur n°%d (couleur %s")
+        printf ("\n\nEntrez le nom du joueur n°%d (couleur %s, 30 caractères max.) : ",i+1, Players[i].couleur);
     }
 }
