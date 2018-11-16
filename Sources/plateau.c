ce7d7621b7bd8d7dcc86cdb6aee9cea123667a25
QuentinLCS <quentin.lechasles@gmail.com>
2018-11-16 21:40:21 +0100

    Plateau : Affichage des chevaux sur les échelles.

--- Sources/plateau.c
+++ Sources/plateau.c
@@ -97,15 +97,34 @@ void affichage_plateau()
             } 
 
             // AFFICHAGE DES ECHELLES
-
+            
             else if (matrice[i][j].couleur[0] == "bleu" && matrice[i][j].echelle) {
-                printf("\033[1;44m %d \033[0m", j);          
+                if (matrice[i][j].nbChevaux) {
+                    printf("\033[1;44m X \033[0m"); 
+                } else {
+                    printf("\033[1;44m %d \033[0m", j);  
+                }        
             } else if (matrice[i][j].couleur[0] == "rouge" && matrice[i][j].echelle) {
-                printf("\033[1;41m %d \033[0m", i);          
+                if (matrice[i][j].nbChevaux) {
+                    printf("\033[1;41m X \033[0m"); 
+                } else {
+                    printf("\033[1;41m %d \033[0m", i);   
+                }     
+                        
             } else if (matrice[i][j].couleur[0] == "jaune" && matrice[i][j].echelle) {
-                printf("\033[1;43m %d \033[0m", TAILLE_PLATEAU - j - 1);          
+                if (matrice[i][j].nbChevaux) {
+                    printf("\033[1;43m X \033[0m"); 
+                } else {
+                    printf("\033[1;43m %d \033[0m", TAILLE_PLATEAU - j - 1);
+                }     
+                          
             } else if (matrice[i][j].couleur[0] == "vert" && matrice[i][j].echelle) {
-                printf("\033[1;42m %d \033[0m", TAILLE_PLATEAU - i - 1);          
+                if (matrice[i][j].nbChevaux) {
+                    printf("\033[1;42m X \033[0m"); 
+                } else {
+                    printf("\033[1;42m %d \033[0m", TAILLE_PLATEAU - i - 1);  
+                }     
+                         
             }
             
             // AFFICHAGE DU CENTRE ( VICTOIRE )
