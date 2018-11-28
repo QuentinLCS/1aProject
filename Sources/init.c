81ca5a74b72239f7fba04be002264fb28079b73a
Nathan <nathan.chavas@gmail.com>
2018-11-28 12:31:36 +0100

    .

--- Sources/init.c
+++ Sources/init.c
@@ -1,25 +1,4 @@
 #include <stdio.h>
-<<<<<<< HEAD
-#include "../Header/pion.h"
-
-void init (){
-    Joueur Players[4];
-    Players[0].num_joueur=1;
-    Players[0].couleur="bleu";
-    Players[1].num_joueur=2;
-    Players[1].couleur="rouge";
-    Players[2].num_joueur=3;
-    Players[2].couleur="jaune";
-    Players[3].num_joueur=4;
-    Players[3].couleur="vert";
-
-
-    int nbJoueur=nbJoueur();
-    for (int i=0; i<nbJoueur; i++){
-        printf ("\n\nEntrez le nom du joueur n°%d (couleur %s, 30 caractères max.) : ",i+1, Players[i].couleur);
-    }
-}
-=======
 #include "../Header/init.h"
 #include "../Header/deplacement.h"
 
@@ -99,4 +78,3 @@ void viderBuffer()
     char c;
     while ( (c=getchar()) != EOF && c != '\n');
 }
->>>>>>> a91af98b06edf1a7ea9b839f21c63e83a826f7a3
