23f3946b409855d86cab07e891e974cca44e1b67
QuentinLCS <quentin.lechasles@gmail.com>
2019-01-03 18:37:36 +0100

    .

--- Sources/plateau.c
+++ Sources/plateau.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "../Header/plateau.h"
 #define TAILLE_PLATEAU 15 // IMPAIR
 #define TAILLE_ECURIE 6 // Calculable ?
@@ -24,22 +25,22 @@ void creationPlateau()
                 matrice[i][j].echelle    = 0;
             } else if (j >= TAILLE_PLATEAU - TAILLE_ECURIE && i < TAILLE_ECURIE) {
                 matrice[i][j].nbChevaux  = 0;      // Ecurie haut droite
-                strcpy(matrice[i][j].couleur, "rouge";
+                strcpy(matrice[i][j].couleur, "rouge");
                 matrice[i][j].ecurie     = 1;
                 matrice[i][j].echelle    = 0;
             } else if (j < TAILLE_ECURIE && i >= TAILLE_PLATEAU - TAILLE_ECURIE) {
                 matrice[i][j].nbChevaux  = 0;      // Ecurie bas gauche
-                strcpy(matrice[i][j].couleur, "vert";
+                strcpy(matrice[i][j].couleur, "vert");
                 matrice[i][j].ecurie     = 1;
                 matrice[i][j].echelle    = 0;
             } else if (j >= TAILLE_PLATEAU - TAILLE_ECURIE && i >= TAILLE_PLATEAU - TAILLE_ECURIE) {
                 matrice[i][j].nbChevaux  = 0;      // Ecurie bas droite
-                strcpy(matrice[i][j].couleur, "jaune";
+                strcpy(matrice[i][j].couleur, "jaune");
                 matrice[i][j].ecurie     = 1;
                 matrice[i][j].echelle    = 0;
             } else if (j == TAILLE_PLATEAU / 2 && i == TAILLE_PLATEAU / 2 && TAILLE_PLATEAU % 2 != 0 || j == TAILLE_PLATEAU / 2 && i == TAILLE_PLATEAU / 2 && TAILLE_PLATEAU % 2 == 0) {
                 matrice[i][j].nbChevaux  = 0;      // Centre du plateau
-               strcpy(matrice[i][j].couleur, " ";
+               strcpy(matrice[i][j].couleur, " ");
                 matrice[i][j].ecurie     = 0;
                 matrice[i][j].echelle    = 0;
             } else if (((j == TAILLE_PLATEAU / 2 && TAILLE_PLATEAU % 2 != 0) || (i == TAILLE_PLATEAU / 2 && TAILLE_PLATEAU % 2 != 0)) && (i != 0 && i != TAILLE_PLATEAU - 1 && j != 0 && j != TAILLE_PLATEAU - 1)) {
@@ -47,17 +48,17 @@ void creationPlateau()
                 matrice[i][j].ecurie    = 0;
                 matrice[i][j].echelle   = 1;
                 if (j == TAILLE_PLATEAU / 2 && TAILLE_PLATEAU % 2 != 0 && i < TAILLE_PLATEAU / 2) {
-                    matrice[i][j].couleur[0] = "rouge";
+                    strcpy(matrice[i][j].couleur, "rouge");
                 } else if (i == TAILLE_PLATEAU / 2 && TAILLE_PLATEAU % 2 != 0 && j < TAILLE_PLATEAU / 2) {
-                    matrice[i][j].couleur[0] = "bleu";
+                    strcpy(matrice[i][j].couleur, "bleu");
                 } else if (i == TAILLE_PLATEAU / 2 && TAILLE_PLATEAU % 2 != 0 && j > TAILLE_PLATEAU / 2) {
-                    matrice[i][j].couleur[0] = "jaune";
+                    strcpy(matrice[i][j].couleur, "jaune");
                 } else { 
-                    matrice[i][j].couleur[0] = "vert";
+                    strcpy(matrice[i][j].couleur, "vert");
                 }   
             } else {
                 matrice[i][j].nbChevaux  = 0; // Chemin utilisable par les chevaux
-                matrice[i][j].couleur[0] = " ";
+                strcpy(matrice[i][j].couleur, " ");
                 matrice[i][j].ecurie     = 0;
                 matrice[i][j].echelle    = 0;
             }
@@ -80,7 +81,7 @@ void affichagePlateau()
 
                 // AFFICHAGE DES ECURIES 
 
-                if (matrice[i][j].couleur[0] == "bleu" && matrice[i][j].ecurie) {
+                if (!strcmp(matrice[i][j].couleur, "bleu") && matrice[i][j].ecurie) {
                     if (matrice[i][j].nbChevaux && k == 1) {
                         printf("\033[1;44m   \u265e  \033[0m"); 
                     } else {
@@ -93,21 +94,21 @@ void affichagePlateau()
                         }
                     }
 
-                } else if (matrice[i][j].couleur[0] == "rouge" && matrice[i][j].ecurie) {
+                } else if (!strcmp(matrice[i][j].couleur, "rouge") && matrice[i][j].ecurie) {
                     if (matrice[i][j].nbChevaux && k == 1) {
                         printf("\033[1;41m  \u265e   \033[0m"); 
                     } else {
                         printf("\033[31;41m      \033[0m");
                     }
 
-                } else if (matrice[i][j].couleur[0] == "jaune" && matrice[i][j].ecurie) {
+                } else if (!strcmp(matrice[i][j].couleur, "jaune") && matrice[i][j].ecurie) {
                     if (matrice[i][j].nbChevaux && k == 1) {
                         printf("\033[1;43m  \u265e   \033[0m"); 
                     } else {
                         printf("\033[33;43m      \033[0m");
                     }
 
-                } else if (matrice[i][j].couleur[0] == "vert" && matrice[i][j].ecurie) {
+                } else if (!strcmp(matrice[i][j].couleur, "vert") && matrice[i][j].ecurie) {
                     if (matrice[i][j].nbChevaux && k == 1) {
                         printf("\033[1;42m   \u265e  \033[0m"); 
                     } else {
@@ -123,7 +124,7 @@ void affichagePlateau()
 
                 // AFFICHAGE DES ECHELLES
                 
-                else if (matrice[i][j].couleur[0] == "bleu" && matrice[i][j].echelle) {
+                else if (!strcmp(matrice[i][j].couleur, "bleu") && matrice[i][j].echelle) {
                     if (k == 1) {
                         if (matrice[i][j].nbChevaux) {
                             printf("\033[1;44m  %d\u265e  \033[0m", j); 
@@ -134,7 +135,7 @@ void affichagePlateau()
                         printf("\033[1;44m      \033[0m");  
                     }    
 
-                } else if (matrice[i][j].couleur[0] == "rouge" && matrice[i][j].echelle) {
+                } else if (!strcmp(matrice[i][j].couleur, "rouge") && matrice[i][j].echelle) {
                     if (k == 1) {
                         if (matrice[i][j].nbChevaux) {
                             printf("\033[1;41m  %d\u265e  \033[0m", i); 
@@ -145,7 +146,7 @@ void affichagePlateau()
                         printf("\033[1;41m      \033[0m");  
                     }    
 
-                } else if (matrice[i][j].couleur[0] == "jaune" && matrice[i][j].echelle) {
+                } else if (!strcmp(matrice[i][j].couleur, "jaune") && matrice[i][j].echelle) {
                     if (k == 1) {
                         if (matrice[i][j].nbChevaux) {
                             printf("\033[1;43m  %d\u265e  \033[0m", TAILLE_PLATEAU - j - 1); 
@@ -156,7 +157,7 @@ void affichagePlateau()
                         printf("\033[1;43m      \033[0m");  
                     }    
 
-                } else if (matrice[i][j].couleur[0] == "vert" && matrice[i][j].echelle) {
+                } else if (!strcmp(matrice[i][j].couleur, "vert") && matrice[i][j].echelle) {
                     if (k == 1) {
                         if (matrice[i][j].nbChevaux) {
                             printf("\033[1;42m  %d\u265e  \033[0m", TAILLE_PLATEAU - i - 1); 
@@ -186,11 +187,11 @@ void affichagePlateau()
                         printf("\033[7m\u02F9    \u02FA\033[0m");
                     } else if (k == 1) {
                         if (matrice[i][j].nbChevaux >= 1) {
-                            if (matrice[i][j].couleur[0] == "bleu") {
+                            if (!strcmp(matrice[i][j].couleur, "bleu")) {
                                 printf("\033[7;44m  %d\u265e  \033[0m", matrice[i][j].nbChevaux);
-                            }  else if (matrice[i][j].couleur[0] == "rouge") {
+                            }  else if (!strcmp(matrice[i][j].couleur, "rouge")) {
                                 printf("\033[7;41m  %d\u265e  \033[0m", matrice[i][j].nbChevaux);
-                            }  else if (matrice[i][j].couleur[0] == "jaune") {
+                            }  else if (!strcmp(matrice[i][j].couleur, "jaune")) {
                                 printf("\033[7;43m  %d\u265e  \033[0m", matrice[i][j].nbChevaux);
                             } else {
                                 printf("\033[7;42m  %d\u265e  \033[0m", matrice[i][j].nbChevaux);
