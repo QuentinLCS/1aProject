b09bc82ca99ed9963ad3bf345ab6c2cb03e96eeb
Chavas <nathan_chavas@nathan.chavas-VirtualBox>
2019-01-06 23:31:01 +0100

    k

--- Sources/deplacement.c
+++ Sources/deplacement.c
@@ -6,7 +6,7 @@
 #include "../Header/deplacement.h"
 #include "../Header/init.h"
 
-int chemin[57][2]=
+int chemin[57][2]= /* Initialisation du chemin du jeu */
 {   
     {14,6}, {13,6}, {12,6}, {11,6}, {10,6}, {9,6}, {8,6}, {8,5}, {8,4}, {8,3}, {8,2}, {8,1}, {8,0}, {7,0}, /*Cases de l'écurie en bas à gauche*/
     {6,0}, {6,1}, {6,2}, {6,3}, {6,4}, {6,5}, {6,6}, {5,6}, {4,6}, {3,6}, {2,6}, {1,6}, {0,6}, {0,7}, /*Cases de l'écurie en haut à gauche*/
@@ -14,7 +14,7 @@ int chemin[57][2]=
     {8,14}, {8,13}, {8,12}, {8,11}, {8,10}, {8,9}, {8,8}, {9,8}, {10,8}, {11,8}, {12,8}, {13,8}, {14,8}, {14,7} /*Cases de l'écurie en bas à droite*/
 } ;
 
-int echelle[4][6][2]=
+int echelle[4][6][2]= /* Initialisation des cases des échelles. Array en 3 dimensions afin de gérer toutes les positions. */
 { 
     {   {13,7}, {12,7}, {11,7}, {10,7}, {9,7}, {8,7}  },
     {   {7,1}, {7,2}, {7,3}, {7,4}, {7,5}, {7,6}   },
@@ -22,33 +22,33 @@ int echelle[4][6][2]=
     {   {7,13}, {7,12}, {7,11}, {7,10}, {7,9}, {7,8}    }
 };
 
-int listeSorties[4][3] = { {6,0,15}, {0,8,29}, {14,6,1}, {8,14,43} };
+int listeSorties[4][3] = { {6,0,15}, {0,8,29}, {14,6,1}, {8,14,43} }; /* Cases correspondantes aux sorties des écuries */
 
-int listeEcuries[4][2] = { {2,2}, {2,11}, {12,2}, {12,11} };
+int listeEcuries[4][2] = { {2,2}, {2,11}, {12,2}, {12,11} }; /* Position des différentes écuries afin de stocker les chevaux */
 
 int de()
 {
     return rand() % 6 + 1;
 }
 
-void initPositionChevaux(int i, int j)
+void initPositionChevaux(int i, int j) /* Initialise la position des chevaux en début de partie dans les écuries */
 {
     players[i].cheval[j].position[0] = listeEcuries[i][0];
     players[i].cheval[j].position[1] = listeEcuries[i][1];
 }
 
-int verification(int numJoueur, int numPion, int distance){
+int verification(int numJoueur, int numPion, int distance){ /*Permet d'attribuer à chaque pion du joueur un état, en fonction des actions disponibles */
     int res=1, i=1;
     if (players[numJoueur].cheval[numPion].numCase < 100)
     {
         while (i<= distance && res==1){
 
-            if (((players[numJoueur].cheval[numPion].numCase -1 + i)%56)+1 == ((listeSorties[numJoueur][2]+54) % 56) + 1) {
+            if (((players[numJoueur].cheval[numPion].numCase -1 + i)%56)+1 == ((listeSorties[numJoueur][2]+54) % 56) + 1) { /* vérifie si le pion dépasse l'échelle */
                 if (i < distance){
                     res = 0;
                 }
             }
-
+            /* Boucle dans le cas où le pion doit passer par une case occupée */
             if (plateau[chemin[ (players[numJoueur].cheval[numPion].numCase -1 + i)%56][0]][chemin[ (players[numJoueur].cheval[numPion].numCase -1 + i)%56][1]].nbChevaux != 0){
                 if (!strcmp(plateau[chemin[ (players[numJoueur].cheval[numPion].numCase -1 + i)%56][0]][chemin[ (players[numJoueur].cheval[numPion].numCase -1 + i)%56][1]].couleur , players[numJoueur].couleur) ){
                     i++;
@@ -62,16 +62,19 @@ int verification(int numJoueur, int numPion, int distance){
                     }
                 }
             }
+            /* Cas par defaut : le chemin est libre et le pion peut s'y déplacer */
             else{
                 i++;
             }  
 
         }
     }
+    /* Si le pion essaye de se déplacer sur une case de l'échelle qu'il occupe déjà */
     else if (players[numJoueur].cheval[numPion].numCase - 100 == distance)
     {
         res=0;
     }
+    /* Une case de l'échelle occupée par un autre pion */
     else if (plateau[echelle[numJoueur][distance-1][0]][echelle[numJoueur][distance-1][0]].nbChevaux != 0)
     {
         res=0;
@@ -95,8 +98,8 @@ void mouvement (int numJoueur, int numPion, int distance)
         deplacement(numJoueur, numPion, distance);
     }
 }
-
-void deplacementEchelle(int numJoueur, int numPion, int distance)
+/* Cette fonction gère le déplacement du pion sur l'échelle */
+void deplacementEchelle(int numJoueur, int numPion, int distance) 
 {
     plateau[players[numJoueur].cheval[numPion].position[0]][players[numJoueur].cheval[numPion].position[1]].nbChevaux = 0;
     strcpy(plateau[players[numJoueur].cheval[numPion].position[0]][players[numJoueur].cheval[numPion].position[1]].couleur, " ");
@@ -107,6 +110,7 @@ void deplacementEchelle(int numJoueur, int numPion, int distance)
     strcpy(plateau[players[numJoueur].cheval[numPion].position[0]][players[numJoueur].cheval[numPion].position[1]].couleur, players[numJoueur].couleur); 
 }
 
+/* Cette fonction gère le déplacement du pion sur le chemin */
 void deplacement(int numJoueur, int numPion, int distance)
 {
     plateau[players[numJoueur].cheval[numPion].position[0]][players[numJoueur].cheval[numPion].position[1]].nbChevaux -= 1;
@@ -147,6 +151,7 @@ void capture(int numJoueur, int numPion, int distance)
     deplacement(numJoueur, numPion, distance);  
 }
 
+/* Cette fonction permet de vérifier si un pion est dans l'écurie. Si il l'est, il recevra l'état passé en paramètre. Sinon, il passera par la fonction Verification afin de connaître l'état dans lequel il se trouve. */
 int checkEcurie(int etat[4], int numEtat, int needVerif, int numJoueur, int distance) 
 {
     int res = 0, chevalTrouve = 0;
@@ -168,7 +173,7 @@ int checkEcurie(int etat[4], int numEtat, int needVerif, int numJoueur, int dist
     }
     return res;
 }
-
+/* Cette fonction permet de gérer la sortie des pions de leur écurie. Si la case de sortie d'écurie est occupée, elle effectura une capture. */
 void sortieEcurie(int etat[4], int numJoueur) 
 {
     int numPion = checkEcurie(etat, 0, 0, numJoueur, 0);
